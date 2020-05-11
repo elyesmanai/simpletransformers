5d7adbe3e9de4d4126703d4e4a5cf76b23bc71b2
Elyes manai <e.manai@instadeep.com>
2020-05-11 14:02:52 +0100

    tqdm

--- prepare_data.py
+++ prepare_data.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import os
+from tqdm import tqdm
 
 all_text = []
 for fl in os.scandir("../data/"):
     if fl.name.endswith(".txt") and "text" in fl.name:
-        df = pd.read_csv(fl, delimiter="\n", header=None)
+        print(f'treating {fl.name}')
+        df = pd.read_csv(fl, columns=["text"], delimiter="\n", header=None)
         texts = df.text.tolist()
-        texts = [t for t in texts if isinstance(t, str)]
+        texts = [t for t in tqdm(texts) if isinstance(t, str)]
         all_text.extend(texts)
 
 train, test = train_test_split(all_text, test_size=0.1)
@@ -16,6 +18,6 @@ with open("../data/train.txt", "w") as f:
     for line in train:
         f.write(line + "\n")
 
-with open("../dada/test.txt", "w") as f:
+with open("../data/test.txt", "w") as f:
     for line in test:
         f.write(line + "\n")
