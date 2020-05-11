cd3b3ded00f6fb074c6ba4d24097c0eecf9a5b57
Elyes manai <e.manai@instadeep.com>
2020-05-11 17:34:57 +0100

    minor rectifications

---
+++ generate.py
@@ -0,0 +1,11 @@
+from transformers import pipeline
+
+fill_mask = pipeline(
+    "fill-mask",
+    model = AutoModelWithLMHead.from_pretrained("outputs/generator_model"),
+    tokenizer= AutoTokenizer.from_pretrained("outputs/generator_model")
+)
+
+print(
+    fill_mask(f"HuggingFace is creating a {nlp.tokenizer.mask_token} that the community uses to solve NLP tasks.")
+)
